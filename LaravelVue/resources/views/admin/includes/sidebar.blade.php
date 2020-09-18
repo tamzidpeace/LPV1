13b7873ed6347da80dc567da0410bf92350dffbe
tamzidpeace <tamjedpeace@gmail.com>
2020-09-18 15:01:54 +0600

    simple admin panel change

--- LaravelVue/resources/views/admin/includes/sidebar.blade.php
+++ LaravelVue/resources/views/admin/includes/sidebar.blade.php
@@ -31,6 +31,26 @@
             </p>
           </a>
         </li>
+        {{-- manage --}} 
+        <li class="nav-item has-treeview menu-open">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-user-cog"></i>
+            <p>
+              Manage
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./index.html" class="nav-link active">
+                <i class="far fa-circle nav-icon"></i>
+                <p>User Info</p>
+              </a>
+            </li>           
+          </ul>
+        </li>
+
+        {{-- manage end --}}
 
         <li class="nav-item ">
           <a href="{{ route('logout.admin') }}" class="nav-link">
