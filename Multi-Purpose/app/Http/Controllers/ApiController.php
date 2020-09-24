14a6a7fe2f265a00f48394df733a0656c8157697
Arafat Kamal <tamjedpeace@gmail.com>
2020-09-24 08:45:53 +0600

    login api

---
+++ Multi-Purpose/app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Customer;
+
+class ApiController extends Controller
+{
+    public function users()
+    {
+        $users = User::all();
+        return \response()->json($users, 200);
+    }
+
+    public function login(Request $request)
+    {
+        $login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($login)) {
+            return \response(['message' => 'invalid login credential!']);
+        } else {
+            $token = Auth::user()->createToken('authTokern')->token;
+
+            return \response(['user' => Auth::user(), 'token' => $token]);
+        }
+    }
+}
--- Multi-Purpose/routes/api.php
+++ Multi-Purpose/routes/api.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
-Route::get('/test3', 'TestController@test3');
+Route::get('users', 'ApiController@users');
+Route::post('login', 'ApiController@login');
