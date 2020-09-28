045d9d74cb15265aecc4e614a0b790ee04f2f8fb
Arafat Kamal <tamjedpeace@gmail.com>
2020-09-28 08:27:20 +0600

    multi tenancy implemented with hyn

--- multi-tenancy/config/database.php
+++ multi-tenancy/config/database.php
@@ -61,7 +61,22 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
+        ],     
+        
+        'system' => [
+            'driver' => 'mysql',
+            'host' => env('TENANCY_HOST', '127.0.0.1'),
+            'port' => env('TENANCY_PORT', '3306'),
+            'database' => env('TENANCY_DATABASE', 'multi_tenancy'),
+            'username' => env('TENANCY_USERNAME', 'root'),
+            'password' => env('TENANCY_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],        
 
         'pgsql' => [
             'driver' => 'pgsql',
