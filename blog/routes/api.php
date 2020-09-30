96abc3f96f8c5e5b470da6fdec6500b58bc5d871
tamzidpeace <tamjedpeace@gmail.com>
2020-09-30 18:12:13 +0600

    cofigured till passport

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('test', 'TestController@index');      
+});
+
+
+Route::post('login', 'TestController@login');
