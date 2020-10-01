dee8691581c44843f2fdc28e83bf33b92b7bdc23
Arafat Kamal <tamjedpeace@gmail.com>
2020-10-02 01:44:00 +0600

    jwt token working

--- mt3/app/Http/Controllers/AuthController.php
+++ mt3/app/Http/Controllers/AuthController.php
@@ -6,36 +6,83 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function __construct()
     {
-        return User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
+    
+    // public function register(Request $request)
+    // {
+    //     return User::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'password' => Hash::make($request->password),
+    //     ]);
+    // }
+
+    // public function login(Request $request)
+    // {
+        
+    //     $login = $request->validate([
+    //         'email' => 'required|string',
+    //         'password' => 'required',
+    //     ]);
+
+    //     if (!Auth::attempt($login)) {
+    //         return \response(['message' => 'invalid login credential!']);
+    //     } else {
+    //         $token = Auth::user()->createToken('authTokern')->accessToken;
+
+    //         return \response(['user' => Auth::user(), 'token' => $token]);
+    //     }
+    // }
+
+    // public function index() {
+    //     return User::all();
+    // }
 
     public function login(Request $request)
     {
-        
-        $login = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required',
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
         ]);
 
-        if (!Auth::attempt($login)) {
-            return \response(['message' => 'invalid login credential!']);
-        } else {            
-            $token = Auth::user()->createToken('authTokern')->accessToken;
-
-            return \response(['user' => Auth::user(), 'token' => $token]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
+
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
     }
 
-    public function index() {
-        return User::all();
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function guard()
+    {
+        return Auth::guard();
+    }
+
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
     }
 }
--- mt3/config/app.php
+++ mt3/config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TenancyServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
@@ -227,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+        'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory'
 
     ],
 
