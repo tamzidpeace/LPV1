518658aa61069f39feb3233b5b82b0e515a5430b
tamzidpeace <tamjedpeace@gmail.com>
2020-10-15 19:03:51 +0600

    twilio api integration successfull

--- ve/app/Http/Controllers/HomeController.php
+++ ve/app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Twilio\Rest\Client;
+use Twilio\Exceptions\TwilioException;
 
 class HomeController extends Controller
 {
@@ -13,7 +16,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -24,5 +27,25 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function qr()
+    {
+        return QrCode::size(150)->color(82, 206, 157)->generate('Sakib Vai!');
+    }
+
+    public function sms()
+    {
+        $recipients = "+8801303792406";
+        $message = "testing sms2";
+        $account_sid = "ACdfd3039d1faac554730b592c95ee0120";
+        $auth_token = "4fa2ba0026a3fcdf82d722e5d1bea0ab";
+        $twilio_number = "+18035605388";
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create(
+            $recipients,
+            ['from' => $twilio_number, 'body' => $message]
+        );
+        return 123;
     }
 }
