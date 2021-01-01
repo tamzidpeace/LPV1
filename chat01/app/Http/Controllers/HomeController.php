c62511f3bb75db5855a9e923917444e5d1965111
tamzidpeace <tamjedpeace@gmail.com>
2021-01-01 20:42:51 +0600

    print with printer id

--- chat01/app/Http/Controllers/HomeController.php
+++ chat01/app/Http/Controllers/HomeController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Rawilk\Printing\Facades\Printing;
-
+use Rawilk\Printing\Contracts\Printer;
+use Rawilk\Printing\Receipts\ReceiptPrinter;
 
 class HomeController extends Controller
 {
@@ -28,12 +29,32 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function testPrint2() {
-        $printerId = Printing::defaultPrinterId();
+    public function testPrint2()
+    {
+        // $printerId = Printing::defaultPrinterId();
         
+        // Printing::newPrintTask()
+        // ->printer($printerId)
+        // ->file('Scan_Info.pdf')
+        // ->send();
+        //69972550
+        //69972549
+
+        $receipt = (string) (new ReceiptPrinter)
+        ->centerAlign()
+        ->text('My heading')
+        ->leftAlign()
+        ->line()
+        ->twoColumnText('Item 1', '2.00')
+        ->twoColumnText('Item 2', '4.00')
+        ->feed(2)
+        ->centerAlign()
+        ->barcode('1234')
+        ->cut();
+
         Printing::newPrintTask()
-        ->printer($printerId)
-        ->file('Scan_Info.pdf')
+        ->printer(69972550)
+        ->content($receipt)
         ->send();
     }
 }
