687c0639e0103e88ff52387e1bbd196825f2808a
tamzidpeace <tamjedpeace@gmail.com>
2021-03-11 21:05:18 +0600

    supscription is done

---
+++ casher_stripe/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/subscription/create', ['as'=>'home','uses'=>'SubscriptionController@index'])->name('subscription.create');
+Route::post('order-post', ['as'=>'order-post','uses'=>'SubscriptionController@orderPost']);
